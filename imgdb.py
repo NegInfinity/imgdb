d5662f61aef16b1ed952e7181a3442b8373123c4
Victor 'NegInfinity' Eremin <altctrlsysrqb@gmail.com>
2021-09-05 22:27:22 +0300

    initial commit

---
+++ imgdb.py
@@ -0,0 +1,431 @@
+import os, json
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from pathlib import Path
+from datetime import datetime
+import hashlib
+
+from PIL import Image
+import dhash
+import pytesseract
+import numpy as np
+
+from sqlalchemy.sql import exists
+
+from itertools import zip_longest
+
+Base = declarative_base()
+
+DB_PATH = 'imgdb.db'
+
+class FileData(Base):
+	__tablename__ = 'files'
+
+	id = Column(Integer, primary_key=True)
+	path = Column(String, unique=True)
+	size = Column(Integer)
+	ctime = Column(DateTime)
+	mtime = Column(DateTime)
+
+	hash = Column(String)
+	def __str__(self) -> str:
+		return "FileData: {{id: {0}, path: '{1}', size: {2}, ctime: {3}, mtime: {4}, hash: {5}}}".format(
+			self.id, self.path, self.size, self.ctime, self.mtime, self.hash
+		)
+
+class ScanFileData(Base):
+	__tablename__ = 'scanfiles'
+
+	id = Column(Integer, primary_key=True)
+	path = Column(String, unique=True)
+	size = Column(Integer)
+	ctime = Column(DateTime)
+	mtime = Column(DateTime)
+
+	def __str__(self) -> str:
+		return "ScanFileData: {{id: {0}, path: '{1}', size: {2}, ctime: {3}, mtime: {4}}}".format(
+			self.id, self.path, self.size, self.ctime, self.mtime
+		)
+
+class DHashData(Base):
+	__tablename__ = 'dhashes'
+	id = Column(Integer, primary_key=True)
+	hash = Column(String)
+	size = Column(Integer)
+
+	hashSize = Column(Integer)
+	dhash = Column(String)
+	def __str__(self) -> str:
+		return "DHashData: {{id: {0}, hash: '{1}', size: {2}, hashSize: {3}, dhash: {4}}}".format(
+			self.id, self.hash, self.size, self.hashSize, self.dhash
+		)
+
+class PaletteData(Base):
+	__tablename__ = 'palettes'
+	id = Column(Integer, primary_key=True)
+	hash = Column(String)
+	size = Column(Integer)
+
+	palette = Column(String)
+	def __str__(self) -> str:
+		return "PaletteData: {{id: {0}, hash: '{1}', size: {2}, palette: {3}}}".format(
+			self.id, self.hash, self.size, self.palette
+		)
+
+class OcrData(Base):
+	__tablename__ = 'ocr'
+	id = Column(Integer, primary_key=True)
+	hash = Column(String)
+	size = Column(Integer)
+
+	lang = Column(String)
+	text = Column(String)
+	def __str__(self) -> str:
+		return "OcrData: {{id: {0}, hash: '{1}', size: {2}, lang: {3}, text: {4}}}".format(
+			self.id, self.hash, self.size, self.lang, self.text
+		)
+
+class Config:
+	KEY_PATHS = 'paths'
+	KEY_TESSCMD = 'tesscmd'
+	KEY_EXCLUDE_PATHS = 'excludePaths'
+	KEY_EXTENSIONS = 'extensions'
+	DEFAULT_PATH = 'imgdbcfg.json'
+	def save(self, path) -> None:
+		with open(path, mode="w", encoding="utf8") as outFile:
+			data = {}
+			data[Config.KEY_PATHS] = self.paths
+			data[Config.KEY_TESSCMD] = self.tesscmd
+			data[Config.KEY_EXCLUDE_PATHS] = self.excludePaths
+			data[Config.KEY_EXTENSIONS] = self.extensions
+
+			json.dump(data, outFile, indent='\t')
+
+	def load(self, path) -> None:
+		with open(path, mode="r", encoding='utf8') as inFile:
+			data = json.load(inFile)
+			self.paths = list(data[Config.KEY_PATHS])
+			self.tesscmd = data[Config.KEY_TESSCMD]
+			self.excludePaths = list(data[Config.KEY_EXCLUDE_PATHS])
+			self.extensions = list(data[Config.KEY_EXTENSIONS])
+
+	def getConfig():
+		path = Config.DEFAULT_PATH
+		result = Config()
+		if os.path.isfile(path):
+			result.load(path)
+			return result
+		result.save(path)
+		return result
+
+	def isExcludedPath(self, path: str):
+		childPath = Path(path)
+		for cur in self.excludePaths:
+			curPath = Path(cur)
+			if childPath.is_relative_to(curPath):
+				return True
+		return False
+
+	def isSupportedExt(self, filePath) -> bool:
+		split = os.path.splitext(filePath)
+		if (len(split) != 2):
+			return False
+		splitExt = split[1].lower()		
+		for curExt in self.extensions:
+			if curExt.lower() == splitExt:
+				return True
+		return False
+
+	def __init__(self) -> None:
+		self.paths = ['img']
+		self.tesscmd = ['tesseract']
+		self.excludePaths = ['img/excluded']
+		self.extensions = ['.png', '.tga', '.jpeg', '.jpg', '.bmp']
+		pass
+	pass
+
+def getDigest(path: str):
+	h = hashlib.sha256()
+	blockSize = 1024 * 128
+	with open(path, "rb") as inFile:
+		while True:
+			chunk = inFile.read(blockSize)
+			if not chunk:
+				break
+			h.update(chunk)
+	return h.hexdigest()
+
+def getDHash(path: str, size: int = 8):
+	with Image.open(path) as img:
+		row, col = dhash.dhash_row_col(img)
+		return dhash.format_hex(row, col)
+
+"""
+letterPalette = [
+	('K', (0x00, 0x00, 0x00)),
+	('W', (0xFF, 0xFF, 0xFF)),
+	('R', (0xFF, 0x00, 0x00)),
+	('G', (0x00, 0xFF, 0x00)),
+	('B', (0x00, 0x00, 0xFF)),
+	('Y', (0xFF, 0xFF, 0x00)),
+	('M', (0xFF, 0x00, 0xFF)),
+	('C', (0x00, 0xFF, 0xFF)),
+	('D', (0x3F, 0x3F, 0x3F)),
+	('A', (0x7F, 0x7F, 0x7F)),
+	('L', (0xBF, 0xBF, 0xBF))
+]
+"""
+letterPalette = [
+	('K', (0x00, 0x00, 0x00)),
+	('W', (0xFF, 0xFF, 0xFF)),
+
+	# ('R', (0x7F, 0x00, 0x00)),
+	# ('G', (0x00, 0x7F, 0x00)),
+	# ('B', (0x00, 0x00, 0x7F)),
+
+	# ('Y', (0x7F, 0x7F, 0x00)),
+	# ('M', (0x7F, 0x00, 0x7F)),
+	# ('C', (0x00, 0x7F, 0x7F)),
+
+	('R', (0x3F, 0x00, 0x00)),
+	('R', (0x7F, 0x00, 0x00)),
+	('R', (0xFF, 0x00, 0x00)),
+	('R', (0x7F, 0x3F, 0x3F)),
+	('R', (0xFF, 0x3F, 0x3F)),
+	('R', (0xFF, 0x7F, 0x7F)),
+
+	('G', (0x00, 0x3F, 0x00)),
+	('G', (0x00, 0x7F, 0x00)),
+	('G', (0x00, 0xFF, 0x00)),
+	('G', (0x3F, 0x7F, 0x3F)),
+	('G', (0x3F, 0xFF, 0x3F)),
+	('G', (0x7F, 0xFF, 0x7F)),
+
+	('B', (0x00, 0x00, 0x3F)),
+	('B', (0x00, 0x00, 0x7F)),
+	('B', (0x00, 0x00, 0xFF)),
+	('B', (0x3F, 0x3F, 0x7F)),
+	('B', (0x3F, 0x3F, 0xFF)),
+	('B', (0x7F, 0x7F, 0xFF)),
+
+	('Y', (0x3F, 0x3F, 0x00)),
+	('Y', (0x7F, 0x7F, 0x00)),
+	('Y', (0xFF, 0xFF, 0x00)),
+	('Y', (0x7F, 0x7F, 0x3F)),
+	('Y', (0xFF, 0xFF, 0x3F)),
+	('Y', (0xFF, 0xFF, 0x7F)),
+
+	('M', (0x3F, 0x00, 0x3F)),
+	('M', (0x7F, 0x00, 0x7F)),
+	('M', (0xFF, 0x00, 0xFF)),
+	('M', (0x7F, 0x3F, 0x7F)),
+	('M', (0xFF, 0x3F, 0xFF)),
+	('M', (0xFF, 0x7F, 0xFF)),
+
+	('C', (0x00, 0x3F, 0x3F)),
+	('C', (0x00, 0x7F, 0x7F)),
+	('C', (0x00, 0xFF, 0xFF)),
+	('C', (0x3F, 0x7F, 0x7F)),
+	('C', (0x3F, 0xFF, 0xFF)),
+	('C', (0x7F, 0xFF, 0xFF)),
+
+	('D', (0x3F, 0x3F, 0x3F)),
+	('A', (0x7F, 0x7F, 0x7F)),
+	('L', (0xBF, 0xBF, 0xBF))
+]
+
+def getPaletteString(path: str):
+	with Image.open(path) as img:
+		palette = []
+		for x in letterPalette:
+			palette.extend(x[1])
+		palette = palette + [0]*(768 - len(palette))
+		
+		palImg = Image.new('P', (1, 1))
+		palImg.putpalette(palette)
+
+		convImg = img.quantize(palette=palImg, dither=0)
+		unique, counts = np.unique(convImg, return_counts=True)
+		total = sum(counts)
+		#print(unique, counts, total)
+		cutoff = total*2/100
+
+		res = [letterPalette[x[0]][0] for x in zip_longest(unique, counts) if x[1] > cutoff]
+		res = "".join(res)
+		#print(res)
+		#convImg.show()
+		return res
+
+def main():
+	config = Config.getConfig()
+	engine = create_engine("sqlite:///{0}".format(DB_PATH))
+	Base.metadata.create_all(engine)
+
+	Session = sessionmaker(bind = engine)
+	session: sqlalchemy.orm.Session = Session()
+	session.query(ScanFileData).delete()
+
+	print("scanning filesystem")
+	for curPath in config.paths:
+		for root, dirs, files in os.walk(curPath, topdown=True):
+			if config.isExcludedPath(root):
+				continue
+			for curFile in files:
+				curFilePath = os.path.join(root, curFile)
+				if not config.isSupportedExt(curFilePath):
+					continue
+				print("scanning: {0}".format(curFilePath))
+				fileObj = Path(curFilePath)
+				if not fileObj.exists:
+					print("File {0} not found".format(curFilePath))
+					continue
+				fileStat = fileObj.stat()
+
+				scanData = ScanFileData(
+					path = curFilePath,
+					size = fileStat.st_size,
+					ctime = datetime.fromtimestamp(fileStat.st_ctime),
+					mtime = datetime.fromtimestamp(fileStat.st_mtime)
+				)
+				session.add(scanData)
+
+	print("scan done, building queries")
+
+	newFiles = session.query(ScanFileData).filter(~ exists().where(FileData.path == ScanFileData.path))
+	deletedFiles = session.query(FileData).filter(~ exists().where(FileData.path == ScanFileData.path))
+	changedFiles = session.query(ScanFileData, FileData).filter(ScanFileData.path == FileData.path).filter(
+		(ScanFileData.size != FileData.size) or 
+		(ScanFileData.mtime != FileData.mtime) or 
+		(ScanFileData.ctime != FileData.ctime)
+	)
+
+	numNewFiles = newFiles.count()
+	print("new files: {0}".format(numNewFiles))
+	numDeletedFiles = deletedFiles.count()
+	print("deleted files: {0}".format(numDeletedFiles))
+	numChangedFiles = changedFiles.count()
+	print("changed files: {0}".format(numChangedFiles))
+
+	print("processing deleted files: {0}".format(deletedFiles.count()))
+	fileIndex = 0
+	for f in deletedFiles.all():
+		fileIndex += 1
+		print("deleting file {0}/{1}".format(fileIndex, numDeletedFiles))
+		session.delete(f)
+
+	print("processing changed files: {0}".format(changedFiles.count()))
+	fileIndex = 0
+	for scanFile, file in changedFiles.all():
+		fileIndex += 1
+		print("processing changed file {1}/{2}: {0}".format(scanFile.path, fileIndex, numNewFiles))
+		file.ctime = scanFile.ctime
+		file.mtime = scanFile.mtime
+		file.size = scanFile.size
+		file.hash = getDigest(scanFile.path)
+
+	print("processing new files: {0}".format(newFiles.count()))
+	fileIndex = 0
+	for scanFile in newFiles.all():
+		fileIndex += 1
+		print("processing new file {1}/{2}: {0}".format(scanFile.path, fileIndex, numNewFiles))
+		newData = FileData(
+			path = scanFile.path,
+			size = scanFile.size,
+			mtime = scanFile.mtime,
+			ctime = scanFile.ctime,
+
+			hash = getDigest(scanFile.path)
+		)
+		session.add(newData)
+
+	print("committing to db")	
+
+	session.commit()
+	print("committed")
+
+	print("building dhashes")
+	missingDHashes = session.query(FileData).filter(~ exists().where(
+		(FileData.hash == DHashData.hash) and (FileData.size == DHashData.size))
+	)
+	print("DHashes missing: {0}".format(missingDHashes.count()))
+	dhashSize = 8
+	fileIndex = 0
+	numFiles = missingDHashes.count()
+	for fileData in missingDHashes.all():
+		fileIndex += 1
+		print("building hash {1}/{2}for: {0}".format(fileData.path, fileIndex, numFiles))
+
+		imgHash = getDHash(fileData.path, dhashSize)
+		newData = DHashData(
+			hash = fileData.hash,
+			size = fileData.size,
+			hashSize = dhashSize,
+			dhash = imgHash
+		)
+		print(newData)
+		session.add(newData)
+
+	print("commiting")
+	session.commit()
+
+	print("ocr")
+	pytesseract.pytesseract.tesseract_cmd = config.tesscmd
+
+	print(pytesseract.get_languages())
+	ocrLang = 'eng'
+	missingOcr = session.query(FileData).filter(~ exists().where(
+		(FileData.hash == OcrData.hash) and (FileData.size == OcrData.size) and (OcrData.lang == ocrLang)) #This is wrong and doesn't work
+	)
+	numFiles = missingOcr.count()
+	print("missing translations: {0}".format(numFiles))
+	fileIndex = 0
+	useFullData = False
+	for fileData in missingOcr.all():
+		fileIndex += 1
+		print("building ocr {1}/{2}for: {0}".format(fileData.path, fileIndex, numFiles))
+
+		with Image.open(fileData.path) as img:
+			ocr = pytesseract.image_to_data(img, lang=ocrLang) if useFullData else pytesseract.image_to_string(img, lang=ocrLang) 
+			#print(ocr)
+			newData = OcrData(
+				size = fileData.size,
+				hash = fileData.hash,
+				lang = ocrLang,
+				text = ocr
+			)
+			print(newData)
+			session.add(newData)
+			
+	print("commiting")
+	session.commit()
+	print('all done')
+
+	missingPal = session.query(FileData).filter(~ exists().where(
+		(FileData.hash == PaletteData.hash) and (FileData.size == PaletteData.size))
+	)
+	numFiles = missingPal.count()
+	fileIndex = 0;
+	print("missing palettes: {0}".format(numFiles))
+	for fileData in missingPal.all():
+		fileIndex += 1
+		print("building palette {1}/{2}for: {0}".format(fileData.path, fileIndex, numFiles))
+
+		palString = getPaletteString(fileData.path)
+		#print(palString)
+		newData = PaletteData(
+			size = fileData.size,
+			hash = fileData.hash,
+			palette = palString
+		)
+		print(newData)
+		session.add(newData)
+
+	session.commit()
+
+	pass
+
+if __name__ == "__main__":
+	main()
